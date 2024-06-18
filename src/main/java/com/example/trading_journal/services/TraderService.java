7b7dc32d734efb530870206374ab69de8d257eed
YNegash <n.yeabsira.a@gmail.com>
2024-06-18 20:41:38 +0100

    starts creating service for Trader

--- src/main/java/com/example/trading_journal/services/TraderService.java
+++ src/main/java/com/example/trading_journal/services/TraderService.java
@@ -1,4 +1,24 @@
 package com.example.trading_journal.services;
 
+import com.example.trading_journal.models.Trader;
+import com.example.trading_journal.repositories.TraderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class TraderService {
+
+    @Autowired
+    TraderRepository traderRepository;
+
+    public List<Trader> getAllTraders(){
+        return traderRepository.findAll();
+    }
+
+    public Optional<Trader> getTraderById(Long id){
+        return traderRepository.findById(id);
+    }
 }
