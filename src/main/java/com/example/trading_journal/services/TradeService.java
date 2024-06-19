31cea6e5a772a7f601c96b98a3dc6355dd853cff
YNegash <n.yeabsira.a@gmail.com>
2024-06-19 18:59:27 +0100

    populates TradeService with needed methods

--- src/main/java/com/example/trading_journal/services/TradeService.java
+++ src/main/java/com/example/trading_journal/services/TradeService.java
@@ -1,4 +1,28 @@
 package com.example.trading_journal.services;
 
+import com.example.trading_journal.models.Trade;
+import com.example.trading_journal.repositories.TradeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class TradeService {
+
+    @Autowired
+    TradeRepository tradeRepository;
+
+    public List<Trade> getAllTrades(){
+        return tradeRepository.findAll();
+    }
+
+    public Optional<Trade> getTradeById(Long id){
+        return tradeRepository.findById(id);
+    }
+
+    public void addTrade(Trade trade){
+        tradeRepository.save(trade);
+    }
 }
