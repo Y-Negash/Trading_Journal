f65957143e278749e845e9b341742c18e5193446
YNegash <n.yeabsira.a@gmail.com>
2024-06-20 23:39:12 +0100

    removes redundant imports and comments

--- src/main/java/com/example/trading_journal/controllers/TraderController.java
+++ src/main/java/com/example/trading_journal/controllers/TraderController.java
@@ -5,7 +5,6 @@ import com.example.trading_journal.models.TraderDTO;
 import com.example.trading_journal.services.TraderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
