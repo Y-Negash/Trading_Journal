3c0ff4edb0c41242517c914cd21150dbbc5a6da5
Y-Negash <n.yeabsira.a@gmail.com>
2024-09-17 13:16:27 +0100

    adds functionality to trade form

--- Frontend/src/pages/Home/HomePage.tsx
+++ Frontend/src/pages/Home/HomePage.tsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import TradeForm from "./TradeForm";
 import './HomePage.css';
 
-const HomePage: React.FC = () => {
-   
+interface Trade{
+    name: string,
+    tradeDate: Date,
+    entryPoint: number,
+    exitPoint: number,
+    stopLoss: number,
+    takeProfit: number,
+}
+
+interface HomePageProps{
+    addTrade: (trade: Trade) => void;
+}
+const HomePage: React.FC<HomePageProps> = ({ addTrade }) => {
+
     return(
         <>
         <div className="container">
             <h1 id="title">Trading Journal</h1>
-            <TradeForm />
+            <TradeForm addTrade={addTrade} />
         </div>
         </>
     );
