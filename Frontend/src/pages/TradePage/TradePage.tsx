b1a475a34727e2e34c4e35bc8048e7281ce601c9
Y-Negash <n.yeabsira.a@gmail.com>
2024-11-01 14:05:52 +0000

    #41 moves trade list into trade page file

--- Frontend/src/pages/TradePage/TradePage.tsx
+++ Frontend/src/pages/TradePage/TradePage.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import TradeList from "./TradeList";
 import { Trade } from '../../interfaces';
 import NavBar from "../../components/navigation/NavBar";
 import './TradePage.css';
@@ -10,10 +9,26 @@ interface TradePageProps{
 
 const TradePage: React.FC<TradePageProps> = ({ trades }) => {
 
+    const mappedTrades = trades.map((trade) => {
+        return (
+                <ul key={trade.id}  className="trade-list">
+                    <h3>{trade.name}</h3>
+                    <hr/>
+                    <p>Entry Point: £{trade.entryPoint}</p>
+                    <p>Exit Point: £{trade.exitPoint}</p>
+                    <p>Stop Loss: £{trade.stopLoss}</p>
+                    <p>Take Profit: £{trade.takeProfit}</p>
+                    <p>{trade.tradeDate}</p>
+                </ul>
+        )
+    })
+
     return(
         <div className="container">
             <NavBar />
-            <TradeList trades={trades} />
+            <div className="trade-container">
+            {mappedTrades}
+            </div>
         </div>
     )
 }
