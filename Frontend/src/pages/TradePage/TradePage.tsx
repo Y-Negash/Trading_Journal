9322a1c44fbf5d6bfd10540245872ecbfac049c9
Y-Negash <n.yeabsira.a@gmail.com>
2024-11-01 16:41:20 +0000

    adds styling to buy/sell banner & trade tiles

--- Frontend/src/pages/TradePage/TradePage.tsx
+++ Frontend/src/pages/TradePage/TradePage.tsx
@@ -11,9 +11,9 @@ const TradePage: React.FC<TradePageProps> = ({ trades }) => {
     
     const getTradeType = (trade: Trade) => {
         if(trade.entryPoint > trade.takeProfit || trade.entryPoint < trade.stopLoss){
-            return "SELL";
+            return <button className="sell">SELL</button>;
         } else {
-            return "BUY";
+            return <button className="buy">BUY</button>;
         }
     }
 
@@ -24,13 +24,31 @@ const TradePage: React.FC<TradePageProps> = ({ trades }) => {
         return (
                 <ul key={trade.id}  className="trade-list">
                     <h3>{trade.name}</h3>
-                    <hr/>
-                    <p>Entry Point: £{trade.entryPoint}</p>
-                    <p>Exit Point: £{trade.exitPoint}</p>
-                    <p>Stop Loss: £{trade.stopLoss}</p>
-                    <p>Take Profit: £{trade.takeProfit}</p>
-                    <p>{trade.tradeDate}</p>
-                    <button>{tradeType}</button>
+                    <p>{tradeType}</p>
+    <div className="details">
+        <div className="detail-item">
+            <p>Entry Point:</p>
+            <span>£{trade.entryPoint}</span>
+        </div>
+        <div className="detail-item">
+            <p>Exit Point:</p>
+            <span>£{trade.exitPoint}</span>
+        </div>
+        <div className="detail-item">
+            <p>Stop Loss:</p>
+            <span>£{trade.stopLoss}</span>
+        </div>
+        <div className="detail-item">
+            <p>Take Profit:</p>
+            <span>£{trade.takeProfit}</span>
+        </div>
+        <hr/>
+        <div className="detail-item">
+            <p>Date:</p>
+            <span>{trade.tradeDate}</span>
+        </div>
+        <hr/>
+    </div>
                 </ul>
         )
     })
