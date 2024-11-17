c4777cb7f169b3cc7dd138b45c9205e107288b1f
Y-Negash <n.yeabsira.a@gmail.com>
2024-11-17 21:48:52 +0000

    creates getTradeByFilterStatus() method

--- Backend/src/main/java/com/example/trading_journal/repositories/TradeRepository.java
+++ Backend/src/main/java/com/example/trading_journal/repositories/TradeRepository.java
@@ -2,8 +2,15 @@ package com.example.trading_journal.repositories;
 
 import com.example.trading_journal.models.Trade;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface TradeRepository extends JpaRepository<Trade, Long> {
+    @Query(value = "SELECT x FROM Trade  x WHERE x.tradeDate >=:startDate")
+    List<Trade> findAllWithStartDateAfter(@Param("startDate") LocalDate startDate);
 }
