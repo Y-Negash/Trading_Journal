8df6a0ea4b1282c842d26088e252fbe6defb4383
Y-Negash <n.yeabsira.a@gmail.com>
2024-11-17 21:04:57 +0000

    creates custom query for filtering and adds  annotation to all repositories

--- Backend/src/main/java/com/example/trading_journal/repositories/TraderRepository.java
+++ Backend/src/main/java/com/example/trading_journal/repositories/TraderRepository.java
@@ -2,6 +2,8 @@ package com.example.trading_journal.repositories;
 
 import com.example.trading_journal.models.Trader;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TraderRepository extends JpaRepository<Trader, Long> {
 }
