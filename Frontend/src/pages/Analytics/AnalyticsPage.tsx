fa1b70d4856211f0f52cf8cea13ceca7a7a6c1b0
Y-Negash <n.yeabsira.a@gmail.com>
2025-03-04 02:47:03 +0000

    displays and styles charts including total trade metric too

--- Frontend/src/pages/Analytics/AnalyticsPage.tsx
+++ Frontend/src/pages/Analytics/AnalyticsPage.tsx
@@ -3,23 +3,47 @@ import NavBar from "../../components/navigation/NavBar";
 import './AnalyticsPage.css';
 import { AnalyticsProps } from "../../interfaces";
 import DoughnutChart from "../../components/charts/DoughnutChart";
-import { calculateWinRate } from "../../utils/tradeUtils";
+import { calculateWinRate, getRiskToRewardRatio  } from "../../utils/tradeUtils";
+import PieChart from "../../components/charts/PieChart";
+import HorizontalBarChart from "../../components/charts/HorizontalBarChart";
 
 const AnalyticsPage:React.FC<AnalyticsProps> = ({trades}) => {
 
+  // Doughnut Chart
   const winRate = calculateWinRate(trades);
   const percentage = winRate + "%";
-   
-  return(
-    <div className="container">
+
+  //Total Trades Chart
+  const totalTrade = trades.length;
+
+  // Risk to Reward Chart
+  const riskToRewardRatio = getRiskToRewardRatio(trades).ratio.toFixed(0);
+   console.log(riskToRewardRatio);
+  
+  return (
+    <div className="analytics-container">
       <NavBar />
       <section className="doughnut chart">
         <p>{percentage}</p>
-        <DoughnutChart trades={trades}/>
+        <DoughnutChart trades={trades} />
         <h2>Win Rate</h2>
       </section>
+      <section className="pie chart">
+        <PieChart  trades={trades} />
+      </section>
+      <section className="total-trades chart">
+        <div>
+          <h2>{totalTrade}</h2>
+          <h3>Total Trades</h3>
+          <p>The number of trades you have currently </p>
+        </div>
+      </section>
+      <section className="horizontal-bar chart">
+        <HorizontalBarChart trades={trades}/>
+        <p>Risk to Reward Ratio: {riskToRewardRatio}</p>
+      </section>
     </div>
-   );
+  );
 }
 
 export default AnalyticsPage;
